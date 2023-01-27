1bfb6433512248244e0467011a03c5395d074a2d
bradenkirkham <89403278+bradenkirkham@users.noreply.github.com>
2023-01-26 22:19:19 -0700

    Delete App directory

--- App/app/src/main/java/com/example/bmr_app/NPS_API/Exception.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bmr_app.NPS_API
-
-data class Exception(
-    val endDate: String,
-    val exceptionHours: ExceptionHours,
-    val name: String,
-    val startDate: String
-)
--- App/app/src/main/java/com/example/bmr_app/NPS_API/NationalParkData.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bmr_app.NPS_API
-
-data class NationalParkData(
-    val `data`: List<Data>,
-    val limit: String,
-    val start: String,
-    val total: String
-)
